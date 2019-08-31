77022054acb0a659b0cf6f74da8dff74621f2e52
17607 <1760779801@qq.com>
2019-08-31 10:32:23 +0800

    SpringBoot

---
+++ src/main/java/com/kpy/springboot/dao/OrderDao.java
@@ -0,0 +1,11 @@
+package com.kpy.springboot.dao;
+
+/**
+ * @auther: kpy
+ * @version: 1.0
+ * @Package: com.kpy.springboot.dao
+ * @data: 2019-8-26 17:40
+ * @discription:
+ **/
+public class OrderDao {
+}
