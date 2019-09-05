034a9c1745b85a1fe7684318dcf01171b4f3ecbb
17607 <1760779801@qq.com>
2019-09-05 14:17:50 +0800

    SpringBoot

--- src/main/java/com/kpy/springboot/SpringbootApplication.java
+++ src/main/java/com/kpy/springboot/SpringbootApplication.java
@@ -2,10 +2,18 @@ package com.kpy.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SpringbootApplication {
+public class SpringbootApplication extends SpringBootServletInitializer {
     public static void main(String[] args) {
         SpringApplication.run(SpringbootApplication.class, args);
+    }
+
+    // SpringBootServletInitializer 实现configure 方便打war 外部服务器部署。
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(SpringbootApplication.class);
     }
 }
